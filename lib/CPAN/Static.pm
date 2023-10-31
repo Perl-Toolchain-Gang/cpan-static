807dc304c474cceba87c07bde7a50f464b56a0bd
Leon Timmermans <fawaka@gmail.com>
2023-10-31 19:45:37 +0100

    Add a CPAN::Static stub module

---
+++ lib/CPAN/Static.pm
@@ -0,0 +1,12 @@
+package CPAN::Static;
+
+use strict;
+use warnings;
+
+1;
+
+# ABSTRACT: Static Installation for CPAN distributions
+
+=head1 DESCRIPTION
+
+This distribution contains a L<description|CPAN::Static::Spec> of static CPAN installation, as well as a reference L<implementation|CPAN::Static::Install> for it.
